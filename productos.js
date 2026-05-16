b58a82f23b7fb6f3fe9a88dfdd58676a56b89fe1
padoma <131073123+padoma@users.noreply.github.com>
2026-05-15 21:04:28 -0400

    Update productos.js

--- productos.js
+++ productos.js
@@ -1,1 +1,77 @@
+const productos = {
 
+"co36": {
+    codigo: "CO36",
+    nombre: "Corazón Alado",
+    imagen: "img/co36.jpg",
+    tipo: "medidas",
+
+    opciones: [
+        {
+            medida: "20 cm",
+            mayor: 2000,
+            unitario: 2800
+        },
+        {
+            medida: "30 cm",
+            mayor: 3300,
+            unitario: 4300
+        }
+    ]
+},
+
+"bienve3": {
+    codigo: "Bienve3",
+    nombre: "Bienvenido en capas",
+    imagen: "img/bienve3.jpg",
+    tipo: "simple",
+
+    descripcion: "27 cm",
+
+    mayor: 3000,
+    unitario: 3800
+},
+
+"tiendavintage1": {
+    codigo: "Tienda vintage1",
+    nombre: "Tienda Vintage",
+    imagen: "img/tienda-vintage1.jpg",
+    tipo: "simple",
+
+    descripcion: "23 cm alto | MDF 3 mm",
+
+    mayor: 5500,
+    unitario: 6300
+},
+
+"cuadronavidad": {
+    codigo: "Cuadro Navideño",
+    nombre: "Cuadro Navideño",
+    imagen: "img/cuadro-navidad.jpg",
+    tipo: "simple",
+
+    descripcion: "23 x 18 cm",
+
+    mayor: 4900,
+    unitario: 5800
+},
+
+"renobase": {
+    codigo: "reno base",
+    nombre: "Reno Grabado",
+    imagen: "img/reno-base.jpg",
+    tipo: "variantes",
+
+    opciones: [
+        {
+            nombre: "MDF 3 mm",
+            precio: 1000
+        },
+        {
+            nombre: "MDF 5,5 mm",
+            precio: 1800
+        }
+    ]
+}
+
+};
